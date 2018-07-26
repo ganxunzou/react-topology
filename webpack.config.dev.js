a3dc4f99cc78ca1fe68ad3f2641864326d7e3dd4
ganxunzou <ganxunzou@163.com>
2018-07-26 13:17:35 +0800

    add css module

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,83 +1,106 @@
-const path = require('path');
-const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const OpenBrowserPlugin = require('open-browser-webpack-plugin');
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const OpenBrowserPlugin = require("open-browser-webpack-plugin");
 
 const config = {
-  entry: [
-    'webpack-hot-middleware/client?reload=true',
-    'react-hot-loader/patch',
-    'webpack/hot/only-dev-server',
-    './src/index.jsx',
+	entry: [
+		"webpack-hot-middleware/client?reload=true",
+		"react-hot-loader/patch",
+		"webpack/hot/only-dev-server",
+		"./src/index.jsx"
+	],
+	devtool: "eval-source-map",
+	devServer: {
+		contentBase: "./dist",
+		hot: true,
+		port: 3000
+	},
+	output: {
+		filename: "[name].bundle.js",
+		path: path.resolve(__dirname, "dist"),
+		chunkFilename: "[name].bundle.js"
+		// sourceMapFilename: '[name].js.map',
+	},
+	resolve: {
+		extensions: [".js", ".jsx"],
+		alias: {
+			"react-resize-svg": path.resolve(__dirname, "./src/index.js")
+		}
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				use: "babel-loader",
+				exclude: /node_modules/
+			},
+			{
+				test: /\.css$/i,
+				exclude: [/node_modules/],
+				use: [
+					{
+						loader: "style-loader"
+					},
+					{
+						loader: "css-loader",
+						options: {
+							modules: true,
+							localIdentName: "[name]__[local]--[hash:base64:5]"
+						}
+					},
+					{
+						loader: "postcss-loader"
+					}
+				]
+			},
+			{
+				test: /\.css$/i,
+				exclude: [/src/],
+				use: [
+					{
+						loader: "style-loader"
+					},
+					{
+						loader: "css-loader",
+						options: {
+							importLoaders: 1
+							// modules: true,
+							// localIdentName: "[name]__[local]--[hash:base64:5]"
+						}
+					}
+				]
+			},
+			{
+				test: /\.(eot|woff|woff2|svg|ttf|png|jpg|jpeg)$/,
+				use: [
+					{
+						loader: "url-loader",
+						options: {
+							limit: 20, // 20K
+							fallback: "file-loader", // default
+							name: "[path][name]-[hash:8].[ext]",
+							// publicPath: 'assets/',
+							outputPath: "./imgs/",
+							useRelativePath: false // true : outputPath 失效
+						}
+					}
+				]
+			}
+		]
+	},
 
-  ],
-  devtool: 'eval-source-map',
-  devServer: {
-    contentBase: './dist',
-    hot: true,
-    port: 3000,
-  },
-  output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    chunkFilename: '[name].bundle.js',
-    // sourceMapFilename: '[name].js.map',
-  },
-  resolve: {
-    extensions: ['.js', '.jsx'],
-    alias:{
-      'react-resize-svg': path.resolve(__dirname, './src/index.js')
-    }
-  },
-  module: {
-    rules: [
-      { test: /\.(js|jsx)$/, use: 'babel-loader', exclude: /node_modules/ },
-      {
-        test: /\.css$/i,
-        use: [{
-          loader: 'css-loader',
-          options: {
-            modules: true,
-            localIdentName: '[name]__[local]--[hash:base64:5]',
-          },
-        }],
-      },
-      {
-        test: /\.less$/i,
-        use: [
-          { loader: 'style-loader'},
-          { loader: 'css-loader'},
-          { loader: 'less-loader', options: { javascriptEnabled: true}}
-        ],
-      },
-      { test: /\.(eot|woff|woff2|svg|ttf|png|jpg|jpeg)$/,
-        use: [
-          {
-            loader: 'url-loader',
-            options: {
-              limit: 20, // 20K
-              fallback: 'file-loader', // default
-              name: '[path][name]-[hash:8].[ext]',
-              // publicPath: 'assets/',
-              outputPath: './imgs/',
-              useRelativePath: false, // true : outputPath 失效
-            },
-          }],
-      },
-    ],
-  },
-
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new HtmlWebpackPlugin({
-      filename: 'index.html',
-      template: './template/index_dev.html',
-      title: 'react-boilerplate-dev',
-      // chunks: ['app'], //指定要加入的entry实例,
-      inject: 'body',
-    }),
-    new OpenBrowserPlugin({url: 'http://localhost:3000/'})
-  ],
+	plugins: [
+		new webpack.HotModuleReplacementPlugin(),
+		new HtmlWebpackPlugin({
+			filename: "index.html",
+			template: "./template/index_dev.html",
+			title: "react-boilerplate-dev",
+			// chunks: ['app'], //指定要加入的entry实例,
+			inject: "body"
+		}),
+		new OpenBrowserPlugin({ url: "http://localhost:3000/" })
+	]
 };
 
 module.exports = config;
