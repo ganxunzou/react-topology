206ec188d445e19b9d3ddeba49758a4784f81b57
ganxunzou <ganxunzou@163.com>
2018-07-30 17:19:11 +0800

    add line calc

--- src/utils/CoordinateCalcUtils.js
+++ src/utils/CoordinateCalcUtils.js
@@ -8,6 +8,10 @@ export default class CoordinateCalcUtils{
     if(lineVo.lineType == LineType.Straight){
       // 直线
       return CoordinateCalcUtils.calcStraightLinePoints(lineVo);
+    } else if(lineVo.lineType == LineType.Polyline1) {
+      return CoordinateCalcUtils.calcPolyline1Points(lineVo);
+    } else if(lineVo.lineType == LineType.Polyline2) {
+      return CoordinateCalcUtils.calcPolyline2Points(lineVo);
     }
   }
 
@@ -16,13 +20,14 @@ export default class CoordinateCalcUtils{
 
     // 直线总共有八种情况
     if(bInfo.tNodeX > (bInfo.fNodeX + bInfo.fNodeW) && bInfo.tNodeY < (bInfo.fNodeY + bInfo.fNodeH) && bInfo.tNodeY > bInfo.fNodeY) {
+      // 1
       let fX = bInfo.fNodeX + bInfo.fNodeW;
       let fY = bInfo.fNodeY + bInfo.fNodeH / 2;
       let tX = bInfo.tNodeX;
       let tY = bInfo.tNodeY + bInfo.tNodeH /2;
       return [fX, fY, tX, tY];
     } else if(bInfo.tNodeX > (bInfo.fNodeX + bInfo.fNodeW) && bInfo.tNodeY < bInfo.fNodeY){
-     
+      // 2
       let fX = bInfo.fNodeX + bInfo.fNodeW;
       let fY = bInfo.fNodeY + bInfo.fNodeH / 2;
       let tX = bInfo.tNodeX + bInfo.tNodeW /2;
@@ -30,41 +35,218 @@ export default class CoordinateCalcUtils{
       
       return [fX, fY, tX, tY];
     } else if(bInfo.tNodeX < (bInfo.fNodeX + bInfo.fNodeW) && bInfo.tNodeX > bInfo.fNodeX && bInfo.tNodeY < bInfo.fNodeY) {
+      // 3
       let fX = bInfo.fNodeX + bInfo.fNodeW / 2;
       let fY = bInfo.fNodeY;
       let tX = bInfo.tNodeX + bInfo.tNodeW /2;
       let tY = bInfo.tNodeY + bInfo.tNodeH;
       return [fX, fY, tX, tY];
     } else if(bInfo.tNodeX < bInfo.fNodeX && (bInfo.tNodeY + bInfo.tNodeH) < bInfo.fNodeY) {
+      // 4
       let fX = bInfo.fNodeX + bInfo.fNodeW / 2;
       let fY = bInfo.fNodeY;
       let tX = bInfo.tNodeX + bInfo.tNodeW;
       let tY = bInfo.tNodeY + bInfo.tNodeH / 2;
       return [fX, fY, tX, tY];
     } else if((bInfo.tNodeX+ bInfo.tNodeW) < bInfo.fNodeX && bInfo.tNodeY < (bInfo.fNodeY + bInfo.fNodeH)){
+      // 5
       let fX = bInfo.fNodeX;
       let fY = bInfo.fNodeY + bInfo.fNodeH / 2;
       let tX = bInfo.tNodeX + bInfo.tNodeW;
       let tY = bInfo.tNodeY + bInfo.tNodeH / 2;
       return [fX, fY, tX, tY];
     } else if((bInfo.tNodeX+ bInfo.tNodeW) < bInfo.fNodeX && bInfo.tNodeY > (bInfo.fNodeY + bInfo.fNodeH)){
+      // 6
       let fX = bInfo.fNodeX;
       let fY = bInfo.fNodeY + bInfo.fNodeH / 2;
       let tX = bInfo.tNodeX + bInfo.tNodeW /2;
       let tY = bInfo.tNodeY;
       return [fX, fY, tX, tY];
     } else if((bInfo.tNodeX+ bInfo.tNodeW) > bInfo.fNodeX && bInfo.tNodeX < (bInfo.fNodeX + bInfo.fNodeW) && bInfo.tNodeY > (bInfo.fNodeY + bInfo.fNodeH)) {
+      // 7
       let fX = bInfo.fNodeX + bInfo.fNodeW / 2;
       let fY = bInfo.fNodeY + bInfo.fNodeH;
       let tX = bInfo.tNodeX + bInfo.tNodeW /2;
       let tY = bInfo.tNodeY;
       return [fX, fY, tX, tY];
     } else if(bInfo.tNodeX > (bInfo.fNodeX + bInfo.fNodeW) && bInfo.tNodeY > (bInfo.fNodeY + bInfo.fNodeH)) {
+      // 8
       let fX = bInfo.fNodeX + bInfo.fNodeW / 2;
       let fY = bInfo.fNodeY + bInfo.fNodeH;
       let tX = bInfo.tNodeX ;
       let tY = bInfo.tNodeY + bInfo.tNodeH /2;
       return [fX, fY, tX, tY];
+    }
+  }
+
+  static calcPolyline1Points(lineVo){
+    let bInfo = CoordinateCalcUtils.baseInfo(lineVo);
+
+    // 一级直线总共有八种情况
+    if(bInfo.tNodeX > (bInfo.fNodeX + bInfo.fNodeW) && bInfo.tNodeY < (bInfo.fNodeY + bInfo.fNodeH) && bInfo.tNodeY > bInfo.fNodeY) {
+      // 1
+      let fX = bInfo.fNodeX + bInfo.fNodeW;
+      let fY = bInfo.fNodeY + bInfo.fNodeH / 2;
+      let tX = bInfo.tNodeX;
+      let tY = bInfo.tNodeY + bInfo.tNodeH /2;
+      return [fX, fY, tX, tY];
+    } else if(bInfo.tNodeX > (bInfo.fNodeX + bInfo.fNodeW) && bInfo.tNodeY < bInfo.fNodeY){
+      // 2
+      let fX = bInfo.fNodeX + bInfo.fNodeW;
+      let fY = bInfo.fNodeY + bInfo.fNodeH / 2;
+      let tX = bInfo.tNodeX + bInfo.tNodeW /2;
+      let tY = bInfo.tNodeY + bInfo.tNodeH;
+
+      // bp : breakPoint
+      let bpX = tX;
+      let bpY = fY;
+      
+      return [fX, fY, bpX, bpY, tX, tY];
+    } else if(bInfo.tNodeX < (bInfo.fNodeX + bInfo.fNodeW) && bInfo.tNodeX > bInfo.fNodeX && bInfo.tNodeY < bInfo.fNodeY) {
+      // 3
+      let fX = bInfo.fNodeX + bInfo.fNodeW / 2;
+      let fY = bInfo.fNodeY;
+      let tX = bInfo.tNodeX + bInfo.tNodeW /2;
+      let tY = bInfo.tNodeY + bInfo.tNodeH;
+      return [fX, fY, tX, tY];
+    } else if(bInfo.tNodeX < bInfo.fNodeX && (bInfo.tNodeY + bInfo.tNodeH) < bInfo.fNodeY) {
+      // 4
+      let fX = bInfo.fNodeX + bInfo.fNodeW / 2;
+      let fY = bInfo.fNodeY;
+      let tX = bInfo.tNodeX + bInfo.tNodeW;
+      let tY = bInfo.tNodeY + bInfo.tNodeH / 2;
+
+      let bpX = fX;
+      let bpY = tY;
+
+      return [fX, fY, bpX, bpY, tX, tY];
+    } else if((bInfo.tNodeX+ bInfo.tNodeW) < bInfo.fNodeX && bInfo.tNodeY < (bInfo.fNodeY + bInfo.fNodeH)){
+      // 5
+      let fX = bInfo.fNodeX;
+      let fY = bInfo.fNodeY + bInfo.fNodeH / 2;
+      let tX = bInfo.tNodeX + bInfo.tNodeW;
+      let tY = bInfo.tNodeY + bInfo.tNodeH / 2;
+      return [fX, fY, tX, tY];
+    } else if((bInfo.tNodeX+ bInfo.tNodeW) < bInfo.fNodeX && bInfo.tNodeY > (bInfo.fNodeY + bInfo.fNodeH)){
+      // 6
+      let fX = bInfo.fNodeX;
+      let fY = bInfo.fNodeY + bInfo.fNodeH / 2;
+      let tX = bInfo.tNodeX + bInfo.tNodeW /2;
+      let tY = bInfo.tNodeY;
+
+      let bpX = tX;
+      let bpY = fY;
+
+      return [fX, fY, bpX, bpY, tX, tY];
+    } else if((bInfo.tNodeX+ bInfo.tNodeW) > bInfo.fNodeX && bInfo.tNodeX < (bInfo.fNodeX + bInfo.fNodeW) && bInfo.tNodeY > (bInfo.fNodeY + bInfo.fNodeH)) {
+      // 7
+      let fX = bInfo.fNodeX + bInfo.fNodeW / 2;
+      let fY = bInfo.fNodeY + bInfo.fNodeH;
+      let tX = bInfo.tNodeX + bInfo.tNodeW /2;
+      let tY = bInfo.tNodeY;
+      return [fX, fY, tX, tY];
+    } else if(bInfo.tNodeX > (bInfo.fNodeX + bInfo.fNodeW) && bInfo.tNodeY > (bInfo.fNodeY + bInfo.fNodeH)) {
+      // 8
+      let fX = bInfo.fNodeX + bInfo.fNodeW / 2;
+      let fY = bInfo.fNodeY + bInfo.fNodeH;
+      let tX = bInfo.tNodeX ;
+      let tY = bInfo.tNodeY + bInfo.tNodeH /2;
+
+      let bpX = fX;
+      let bpY = tY;
+
+      return [fX, fY, bpX, bpY, tX, tY];
+    }
+  }
+
+  static calcPolyline2Points(lineVo){
+    let bInfo = CoordinateCalcUtils.baseInfo(lineVo);
+     // 二级直线总共有八种情况
+     if(bInfo.tNodeX > (bInfo.fNodeX + bInfo.fNodeW) && bInfo.tNodeY < (bInfo.fNodeY + bInfo.fNodeH) && bInfo.tNodeY > bInfo.fNodeY) {
+      // 1
+      let fX = bInfo.fNodeX + bInfo.fNodeW;
+      let fY = bInfo.fNodeY + bInfo.fNodeH / 2;
+      let tX = bInfo.tNodeX;
+      let tY = bInfo.tNodeY + bInfo.tNodeH /2;
+      return [fX, fY, tX, tY];
+
+    } else if(bInfo.tNodeX > (bInfo.fNodeX + bInfo.fNodeW) && bInfo.tNodeY < bInfo.fNodeY){
+      // 2
+      let fX = bInfo.fNodeX + bInfo.fNodeW;
+      let fY = bInfo.fNodeY + bInfo.fNodeH / 2;
+      let tX = bInfo.tNodeX;
+      let tY = bInfo.tNodeY + bInfo.tNodeH /2;
+
+      // bp : breakPoint
+      let bp1X = fX + (tX - fX)/2;
+      let bp1Y = fY;
+      let bp2X = fX + (tX - fX)/2;
+      let bp2Y = tY;
+
+      return [fX, fY, bp1X, bp1Y, bp2X, bp2Y, tX, tY];
+    } else if(bInfo.tNodeX < (bInfo.fNodeX + bInfo.fNodeW) && bInfo.tNodeX > bInfo.fNodeX && bInfo.tNodeY < bInfo.fNodeY) {
+      // 3
+      let fX = bInfo.fNodeX + bInfo.fNodeW / 2;
+      let fY = bInfo.fNodeY;
+      let tX = bInfo.tNodeX + bInfo.tNodeW /2;
+      let tY = bInfo.tNodeY + bInfo.tNodeH;
+      return [fX, fY, tX, tY];
+    } else if(bInfo.tNodeX < bInfo.fNodeX && (bInfo.tNodeY + bInfo.tNodeH) < bInfo.fNodeY) {
+      // 4
+      let fX = bInfo.fNodeX + bInfo.fNodeW / 2;
+      let fY = bInfo.fNodeY;
+      let tX = bInfo.tNodeX + bInfo.tNodeW / 2;
+      let tY = bInfo.tNodeY + bInfo.tNodeH;
+
+      // bp : breakPoint
+      let bp1X = fX;
+      let bp1Y = fY + (tY - fY)/2;
+      let bp2X = tX;
+      let bp2Y = fY + (tY - fY)/2;
+
+      return [fX, fY, bp1X, bp1Y, bp2X, bp2Y, tX, tY];
+    } else if((bInfo.tNodeX+ bInfo.tNodeW) < bInfo.fNodeX && bInfo.tNodeY < (bInfo.fNodeY + bInfo.fNodeH)){
+      // 5
+      let fX = bInfo.fNodeX;
+      let fY = bInfo.fNodeY + bInfo.fNodeH / 2;
+      let tX = bInfo.tNodeX + bInfo.tNodeW;
+      let tY = bInfo.tNodeY + bInfo.tNodeH / 2;
+      return [fX, fY, tX, tY];
+    } else if((bInfo.tNodeX+ bInfo.tNodeW) < bInfo.fNodeX && bInfo.tNodeY > (bInfo.fNodeY + bInfo.fNodeH)){
+      // 6
+      let fX = bInfo.fNodeX;
+      let fY = bInfo.fNodeY + bInfo.fNodeH / 2;
+      let tX = bInfo.tNodeX + bInfo.fNodeW;
+      let tY = bInfo.tNodeY + bInfo.tNodeH /2;
+
+      // bp : breakPoint
+      let bp1X = fX + (tX - fX)/2;
+      let bp1Y = fY;
+      let bp2X = fX + (tX - fX)/2;
+      let bp2Y = tY;
+
+      return [fX, fY, bp1X, bp1Y, bp2X, bp2Y, tX, tY];
+    } else if((bInfo.tNodeX+ bInfo.tNodeW) > bInfo.fNodeX && bInfo.tNodeX < (bInfo.fNodeX + bInfo.fNodeW) && bInfo.tNodeY > (bInfo.fNodeY + bInfo.fNodeH)) {
+      // 7
+      let fX = bInfo.fNodeX + bInfo.fNodeW / 2;
+      let fY = bInfo.fNodeY + bInfo.fNodeH;
+      let tX = bInfo.tNodeX + bInfo.tNodeW /2;
+      let tY = bInfo.tNodeY;
+      return [fX, fY, tX, tY];
+    } else if(bInfo.tNodeX > (bInfo.fNodeX + bInfo.fNodeW) && bInfo.tNodeY > (bInfo.fNodeY + bInfo.fNodeH)) {
+      // 8
+      let fX = bInfo.fNodeX + bInfo.fNodeW / 2;
+      let fY = bInfo.fNodeY + bInfo.fNodeH;
+      let tX = bInfo.tNodeX + bInfo.fNodeW / 2;
+      let tY = bInfo.tNodeY;
+
+       // bp : breakPoint
+       let bp1X = fX;
+       let bp1Y = fY + (tY - fY)/2;
+       let bp2X = tX;
+       let bp2Y = fY + (tY - fY)/2;
+
+      return [fX, fY, bp1X, bp1Y, bp2X, bp2Y, tX, tY];
     }
   }
 
